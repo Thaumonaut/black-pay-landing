b3c9303fe97bb6f6d64237817bbced5bebd5786a
Jacob Perry <perryjay123@live.com>
2020-03-10 12:26:21 -0700

    Fixed style issues

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,20 +1,24 @@
 import React from 'react'
-import Img from 'gatsby-image'
+import Img from 'gatsby-image/withIEPolyfill'
 
 import styles from './hero.module.css'
 
-export default({data, img, scrollTo}, props) => (
+export default ({ data, img, scrollTo }, props) => (
   <div className={styles.hero}>
-    <Img fluid={img} className={styles.logo}/>
+    <Img fluid={img} className={styles.logo} />
     <div className={styles.container}>
       <Img
         className={styles.heroImage}
         alt={data.heroImage.title}
-        fluid={data.heroImage.fluid}/>
+        objectFit="contain"
+        fluid={data.heroImage.fluid}
+      />
       <div className={styles.heroDetails}>
         <h1 className={styles.heroHeadline}>{data.heading}</h1>
         <h3 className={styles.heroTitle}>{data.subHeading}</h3>
-        <button className={styles.button} onClick={() => scrollTo()}>Sign Up</button>
+        <button className={styles.button} onClick={() => scrollTo()}>
+          Sign Up
+        </button>
       </div>
     </div>
   </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {graphql} from 'gatsby'
+import { graphql } from 'gatsby'
 import Img from 'gatsby-image'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
@@ -8,17 +8,17 @@ import Layout from '../components/layout'
 import styles from './index.module.css'
 
 class RootIndex extends React.Component {
-
   constructor(props) {
-    super(props);
-    this.forms = React.createRef();
+    super(props)
+    this.forms = React.createRef()
   }
 
   scrollToElement() {
-    this
-      .forms
-      .current
-      .scrollIntoView({behavior: 'smooth', block: 'center', inline: 'center'});
+    this.forms.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'center',
+      inline: 'center',
+    })
   }
 
   render() {
@@ -26,16 +26,26 @@ class RootIndex extends React.Component {
     const testQuery = get(this, 'props.data.contentfulAsset')
     const hero = get(this, 'props.data.contentfulHeroSection')
 
-    const bulletPoints = get(this, 'props.data.contentfulFeatureList.bulletPoint')
-    const partners = get(this, 'props.data.contentfulPartnersList.partnerImages')
+    const bulletPoints = get(
+      this,
+      'props.data.contentfulFeatureList.bulletPoint'
+    )
+    const partners = get(
+      this,
+      'props.data.contentfulPartnersList.partnerImages'
+    )
     const panels = get(this, 'props.data.allContentfulFeaturePanel.edges')
 
     return (
       <Layout location={this.props.location}>
         <div>
-          <Helmet title={siteTitle}/>
-          <Hero data={hero} img={testQuery.fluid} scrollTo={() => this.scrollToElement()}/> {/* Move to Components later */}
-
+          <Helmet title={siteTitle} />
+          <Hero
+            data={hero}
+            img={testQuery.fluid}
+            scrollTo={() => this.scrollToElement()}
+          />{' '}
+          {/* Move to Components later */}
           {/* Bullet Points */}
           <div className={styles.bulletPoints}>
             <h2>Why Black Pay?</h2>
@@ -45,43 +55,69 @@ class RootIndex extends React.Component {
                   <img
                     src={point.icon.file.url}
                     style={{
-                    maxWidth: "100px"
-                  }}/>
+                      maxWidth: '100px',
+                    }}
+                  />
                   <h3>{point.heading}</h3>
                   <p>{point.description.description}</p>
                 </div>
               ))}
             </div>
           </div>
-
           {/* Partner List */}
           <div className={styles.partnerList}>
             <h2>Our Partners</h2>
             <div className={styles.listContainer}>
-              {partners.map((p, i) => (<img src={p.file.url} key={i}/>))}
+              {partners.map((p, i) => (
+                <img src={p.file.url} key={i} />
+              ))}
             </div>
           </div>
-
           {/* Feature Panels */}
-          {panels.map(({
-            node
-          }, i) => (
-              <div 
-                id="panel" key={i}
-                className={styles.panelContainer}
-              >
+          {panels.map(({ node }, i) => (
+            <div id="panel" key={i} className={styles.panelContainer}>
               <div className={styles.panelImage}>
-                <Img fluid={node.image.fluid}/>
+                <Img className={styles.Image} fluid={node.image.fluid} />
               </div>
               <div className={styles.panelText}>
                 <h2>{node.title}</h2>
                 <p>{node.description.description}</p>
               </div>
             </div>
-            ))}
-          
+          ))}
           <div className={styles.formsWrapper} ref={this.forms}>
-            <h2>Coming Soon!</h2>
+            <h2>Contact</h2>
+            <form
+              name="contact"
+              method="post"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+            >
+              {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+              <input type="hidden" name="form-name" value="contact" />
+              <p hidden>
+                <label>
+                  Don’t fill this out: <input name="bot-field" />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Your name:
+                  <br />
+                  <input type="text" name="name" />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Your email:
+                  <br />
+                  <input type="email" name="email" />
+                </label>
+              </p>
+              <p>
+                <button type="submit">Send</button>
+              </p>
+            </form>
           </div>
         </div>
       </Layout>
@@ -91,7 +127,7 @@ class RootIndex extends React.Component {
 
 export default RootIndex
 
-export const pageQuery = graphql `
+export const pageQuery = graphql`
   query HomeQuery {
     site {
       siteMetadata {
@@ -123,16 +159,14 @@ export const pageQuery = graphql `
       heading
       heroImage {
         title
-        fluid(
-          maxHeight: 900
-          ) {
-          ...GatsbyContentfulFluid_tracedSVG
+        fluid {
+          ...GatsbyContentfulFluid
         }
       }
       subHeading
     }
 
-    contentfulAsset(contentful_id: {eq: "24qiOp5FdnMYUMA7ACzwA4"}) {
+    contentfulAsset(contentful_id: { eq: "24qiOp5FdnMYUMA7ACzwA4" }) {
       id
       title
       fluid {
@@ -178,7 +212,5 @@ export const pageQuery = graphql `
         }
       }
     }
-
-
   }
 `
